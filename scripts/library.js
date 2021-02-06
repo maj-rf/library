7ccaf089b10231d47d692fd71c512501960598d8
bananabread08 <rmkfermin@gmail.com>
2021-02-06 12:31:41 +0800

    added addBook functionality but not working, added form to add books

--- scripts/library.js
+++ scripts/library.js
@@ -10,9 +10,20 @@ function Book(title, author, pages, readStatus){
     }
 }
 
-const book1 = new Book("Noli Me Tangere", "Dr. Jose P. Rizal", 300, "Unread");
-book1.info();
-
-function addBooks(){
-
+function addBooks(e){
+    const bookNum = new Book(e.elements[0].value,e.elements[1].value, e.elements[2].value, e.elements[3].value);
+    bookNum.info();
+    e.preventDefault();
 }
+
+const book1 = new Book("Noli Me Tangere", "Dr. Jose P. Rizal", 300, "Unread");
+const form = document.getElementById("book-form");
+// const display =  document.querySelector(".display");
+// const titleN = document.getElementById("#title");
+// const authorN = document.getElementById("#author");
+// const pagesN = document.getElementById("#pages");
+// const readStatus = document.getElementById("#read-status");
+
+form.addEventListener("submit", addBooks);
+//book1.info();
+
