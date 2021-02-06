0bf426c6473a48bcf2419cf40a0e508087bad05d
bananabread08 <rmkfermin@gmail.com>
2021-02-06 23:27:32 +0800

    addBook functionality is working, working on the displayBooks functionality

--- scripts/library.js
+++ scripts/library.js
@@ -1,29 +1,31 @@
 let myLibrary = [];
 
-function Book(title, author, pages, readStatus){
+function Book(title, author, pages, readStatus){ //constructor
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
-    this.info = function() {
-        return console.log(`${title} by ${author}, ${pages}, ${readStatus}`);
-    }
+}
+
+Book.prototype.info = function(){
+    //display.appendChild(newlyAdded);
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.readStatus}`;
 }
 
 function addBooks(e){
-    const bookNum = new Book(e.elements[0].value,e.elements[1].value, e.elements[2].value, e.elements[3].value);
-    bookNum.info();
     e.preventDefault();
+    newlyAdded = document.createElement("p");
+    const newBook = new Book(inputs.item(0).value, inputs.item(1).value, inputs.item(2).value, inputs.item(3).value);
+    myLibrary.push(newBook);
+    newlyAdded.textContent = newBook.info();
+    display.appendChild(newlyAdded);
+    form.reset();
 }
 
-const book1 = new Book("Noli Me Tangere", "Dr. Jose P. Rizal", 300, "Unread");
-const form = document.getElementById("book-form");
-// const display =  document.querySelector(".display");
-// const titleN = document.getElementById("#title");
-// const authorN = document.getElementById("#author");
-// const pagesN = document.getElementById("#pages");
-// const readStatus = document.getElementById("#read-status");
+//const book1 = new Book("Noli Me Tangere", "Dr. Jose P. Rizal", 300, "Unread");
+const form = document.forms[0];
+const inputs = form.querySelectorAll("input");
+const display = document.querySelector(".display");
 
 form.addEventListener("submit", addBooks);
-//book1.info();
 
