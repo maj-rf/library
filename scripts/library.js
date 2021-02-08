ddd3589a50d4ca1ef8c57731ee6579568fc673f4
bananabread08 <rmkfermin@gmail.com>
2021-02-08 16:37:55 +0800

    fixed styling, added remove Book functionality. partially working read-status toggle

--- scripts/library.js
+++ scripts/library.js
@@ -1,7 +1,7 @@
 //variables
 let myLibrary = [];
 const form = document.getElementById("book-form");
-
+const bookList = document.querySelector("#book-list");
 
 //functions
 function Book(title, author, pages, readStatus){ //constructor
@@ -11,52 +11,61 @@ function Book(title, author, pages, readStatus){ //constructor
     this.readStatus = readStatus;
 }
 
-function addBooks(book){
-    myLibrary.push(book);
+function addBooks(e){
+    e.preventDefault();
+    const title = document.getElementById("title").value; //get values
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const status = document.getElementById("read-status").value;
+    const newBook = new Book(title, author, pages, status); //create new Books using the constructor
+    myLibrary.push(newBook); //add books to array
+    displayBooks(myLibrary); // display all books from array
+    form.reset(); //reset text fields
 }
 
 function displayBooks(myLib){
-    const bookList = document.querySelector("#book-list");
     newlyAdded = document.createElement("tr");
     myLib.forEach(books =>{ //create a row for every books object
         newlyAdded.innerHTML = `
         <td>${books.title}</td>
         <td>${books.author}</td>
         <td>${books.pages}</td>
-        <td>${books.readStatus}</td>
+        <td><button class="toggle" id="rstatus">${books.readStatus}</button></td>
         <td><button class="delete" id="remove"> Remove </button></td>
         `;
         bookList.appendChild(newlyAdded);
-        // const remove = document.querySelector("#remove");
-        // remove.addEventListener(function(){
-        //}
         });
 
 }
 
+function updateBook(e){
+    console.log(e.target);
+    const item = e.target;
+    if(item.classList[0] === "delete"){
+        const itemcont = item.parentElement;
+        itemcont.parentElement.remove();
+    }
+    if(item.classList[0] === "toggle"){ //toggle not working atm
+        const itemcont2 = item.parentElement;
+        if(itemcont2.value = "Read"){
+            itemcont2.innerHTML= `<td><button class="toggle" id="rstatus">Unread</button></td>`;
+            itemcont2.value = "Unread";
+        }
+        else if(itemcont2.value = "Unread"){
+            itemcont2.innerHTML= `<td><button class="toggle" id="rstatus">Read</button></td>`;
+            itemcont2.value = "Read";
+        }
+    }
+}
+
 function openForm() {
     document.getElementById("addbook").style.display = "block";
-  }
+}
   
-  function closeForm() {
+function closeForm() {
     document.getElementById("addbook").style.display = "none";
-  }
-
+}
 
 //events
-form.addEventListener("submit", function(e){
-    e.preventDefault();
-    const title = document.getElementById("title").value; //get values
-    const author = document.getElementById("author").value;
-    const pages = document.getElementById("pages").value;
-    const status = document.getElementById("read-status").value;
-
-    const newBook = new Book(title, author, pages, status); //create new Books using the constructor
-    addBooks(newBook); //add Books to Library
-    displayBooks(myLibrary); // display all books from Library
-
-    form.reset(); //reset text fields
-});
-
-
-
+form.addEventListener("submit", addBooks);
+bookList.addEventListener("click", updateBook);
